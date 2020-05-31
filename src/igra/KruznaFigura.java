9eb71cfdf2e7c9051c8ea8be95c832d4d412ac4b
DavidT58 <cd180700d@student.etf.bg.ac.rs>
2020-05-31 14:51:25 +0200

    removed debugging console output for further debugging purpouses

--- src/igra/KruznaFigura.java
+++ src/igra/KruznaFigura.java
@@ -20,13 +20,13 @@ public class KruznaFigura extends Krug {
 	
 	public synchronized void prosloVreme(double t) {
 		vremeOdStvaranja += t;
-		System.out.println("Pre pomeranja: " + polozajCentra.getX() + " " + polozajCentra.getY());
-		System.out.println("Vekt Brzine pre: " + vektorBrzine.getX() + " " + vektorBrzine.getY());
-		System.out.println("Vreme / 1000 " + t/1000);
+		//System.out.println("Pre pomeranja: " + polozajCentra.getX() + " " + polozajCentra.getY());
+		//System.out.println("Vekt Brzine pre: " + vektorBrzine.getX() + " " + vektorBrzine.getY());
+		//System.out.println("Vreme / 1000 " + t/1000);
 		Vektor pomeraj = vektorBrzine.pomnozi(t/1000);
-		System.out.println("Vekt Brzine posle: " + vektorBrzine.getX() + " " + vektorBrzine.getY());
+		//System.out.println("Vekt Brzine posle: " + vektorBrzine.getX() + " " + vektorBrzine.getY());
 		polozajCentra = polozajCentra.saberi(pomeraj);
-		System.out.println("Posle pomeranja: " + polozajCentra.getX() + " " + polozajCentra.getY());
+		//System.out.println("Posle pomeranja: " + polozajCentra.getX() + " " + polozajCentra.getY());
 	}
 	
 }
